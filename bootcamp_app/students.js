990516f8d6372035a333c30dd1e38abe8ff8f54e
Travis <travisliu708@gmail.com>
2022-09-21 13:53:53 -0700

    change to parameterized queries to prevent sql injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -5,7 +5,7 @@ const pool = require('./database/connection');
 const cohort = process.argv[2];
 const numResults = process.argv[3];
 
-pool.query(`SELECT students.id, students.name, cohorts.name AS cohort_name FROM students JOIN cohorts ON students.cohort_id = cohorts.id WHERE cohorts.name LIKE '%${cohort}%' LIMIT ${numResults}`)
+pool.query(`SELECT students.id, students.name, cohorts.name AS cohort_name FROM students JOIN cohorts ON students.cohort_id = cohorts.id WHERE cohorts.name LIKE $1 LIMIT $2`, [`%${cohort}%`, numResults])
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,13 +1,14 @@
 const pool = require('./database/connection');
 
+
 pool.query(`SELECT DISTINCT teachers.name AS teacher, cohorts.name
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON studentS.id = student_id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
-ORDER BY teachers.name;`)
+ORDER BY teachers.name;`, [process.argv[2]])
   .then(res => {
     console.log('connected');
     res.rows.forEach(row => {
